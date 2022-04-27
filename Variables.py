ed70f63abc763484d0454db0524edbbeefd3d0dd
geethashivanandh <gita.mech295@gmail.com>
2022-04-27 11:56:10 +0530

    fst commit

---
+++ Variables.py
@@ -0,0 +1,23 @@
+print("Hello world")
+
+#print("Geetha")
+name = "Geetha"
+print(name)
+
+#SUM
+i = 10
+j = 11
+print(i+j)
+
+#INDEXING AND SLICING
+name = "GeethaShivananda" #0,1,2,3   0,-4,-3,-2,-1
+print(name[1])
+
+#SLICING
+print(name[0:4]) #4th-1 value will be given
+print(name[:5]) #if u dont mention anything it will take from zeroth position
+print(name[6:]) #if i dont mention anything at end it vl print till end
+print(name[::-1]) #TO PRINT FROM BACK
+print(name[::3]) #To SKIP THE LETTERS AND PRINT
+#TO KNOE THE LENGTH OF TE NAME
+print(len(name))
